982ac57f2a61e0a95b2118ce4e61d2d66d1d4977
Sarah <segrissom@randolphcollege.edu>
2016-12-13 22:45:10 -0500

    Update convertToUniversalTime.m

--- convertToUniversalTime.m
+++ convertToUniversalTime.m
@@ -1,8 +1,33 @@
-function univTime=convertToUniversalTime(time_zone)
+function univTime=convertToUniversalTime(time_zone,country)
+
 if time_zone=='Atlantic Daylight'
     %https://www.mathworks.com/help/matlab/ref/strfind.html
-elseif time_zone=='Atlantic Standard'
-elseif time_zone==
+elseif time_zone=='Atlantic Standard' or 'Eastern Daylight'
+    utc=
+elseif time_zone=='Eastern Standard' or 'Central Daylight'
+    if country=='America' or 'USA' or 'United States'
+    else continue 
+elseif time_zone=='Central Standard' or 'Mountain Daylight'
+    if country=='America' or 'USA' or 'United States'
+    else continue
+elseif time_zone=='Mountain Standard' or 'Pacific Daylight'
+elseif time_zone=='Pacific Standard' or 'Alaska Daylight'
+elseif time_zone=='Alaska Standard'
+elseif time_zone=='Hawaii-Aleutian Standard'
+elseif time_zone=='Samoa Standard'
+elseif time_zone=='Greenwich Mean'
+elseif time_zone=='British Summer' or 'Central Europe'
+elseif time_zone=='Central European Summer' or 'Eastern European'
+elseif time_zone=='Eastern European Summer' or 'Charlie' 
+elseif time_zone=='Delta'
+elseif time_zone=='Western Standard' 
+elseif time_zone=='Western Summer'
+elseif time_zone=='Central Standard'
+elseif time_zone=='Central Summer'
+elseif time_zone=='Eastern'
+elseif time_zone=='Eastern Summer'
+%I can add Asia and Africa later - these are the current most likely I guess
+
     %http://earthsky.org/astronomy-essentials/universal-time
     %https://sites.google.com/site/physics135/Home/announcements/universaltimetolocalsiderealtimeconversion
     %http://www.setileague.org/askdr/lmst.htm
