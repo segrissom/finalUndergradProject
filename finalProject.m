5e2fcd2facd0e0a11e7ab12c852f8c50e6c6a5ff
Sarah <segrissom@randolphcollege.edu>
2016-12-14 19:42:00 -0500

    Update finalProject.m

--- finalProject.m
+++ finalProject.m
@@ -8,16 +8,16 @@ latitude=input('Please insert the latitude ');
 longitude=input('Please insert your longitude ');
 %the angle that the star paths make with respect to the horizon
 celEquator=90-latitude;
-%need time and date in order to determine which stars are visible 
-tme=clock;
-year=tme(1);
-month=tme(2);
-day=tme(3);
-hour=tme(4);
-min=tme(5);
-sec=tme(6);
+%need to calculate the universal time
+country=input('What country are you in?' );
+timezone=input('What is your time zone? ');
+utc=convertToUniversalTime(time_zone,country);
 %need julian date in order to get what stars are in where
+%need to pull out the things in order to do the julDay
+utchour=utc(1);
+mon=utc(2);
+day=utc(3);
+misc=datetime(Y,M,D,H,MI,S);
+year=misc(1);
 %http://aa.usno.navy.mil/faq/docs/GAST.php
 julDay=gregToJulian(month,day,year);
-%need to know what Universal Time is
-timezone=input('What is your time zone? ');
